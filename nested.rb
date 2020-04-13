33cddb57b174c145201a44cae2f0cec47630c64c
Jo L <joseph@josephchilin.com>
2020-04-13 16:51:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
       }
     }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash
 end
 
 def adding_matz
